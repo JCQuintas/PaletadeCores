83fb383fabff5766d339b103b469e5362b7595c7
Jose C Quintas Jr <juniorquintas@gmail.com>
2016-08-01 16:00:19 -0300

    Limpeza

--- js/app.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    'use strict';
-    angular.module('myApp', [
-      'ui.router',
-      'ng-token-auth'
-    ]);
-
-})();
--- js/filtro.js
+++
@@ -1,7 +0,0 @@
-$('.filtro').click(function(){
-	$(this).toggleClass("filtro-selecionado");
-});
-
-// $('.filtro-selecionado').click(function(){
-// 	$(this).removeClass("filtro-selecionado");
-// });
