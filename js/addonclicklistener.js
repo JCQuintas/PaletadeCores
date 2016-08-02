8d302d26d6f55de6ab41062d28e5956d03c13d5d
Jose C Quintas Jr <juniorquintas@gmail.com>
2016-08-02 01:27:58 -0300

    sucesso!

--- js/addonclicklistener.js
+++ js/addonclicklistener.js
@@ -1,21 +1,38 @@
-classname = document.getElementsByClassName("colorcard");
+var newtext = "copiado!"
+var oldElement;
+var oldText;
 
-alert(classname);
-console.log(classname);
-console.log(classname.keys);
-
-var myFunction = function() {
-    var attribute = this.getAttribute("data-hex");
-    alert(attribute);
-};
-
-for (var i = 0; i < classname.length; i++) {
-  alert("yup");
-    if (element1.addEventListener) {  // all browsers except IE before version 9
-      classname[i].addEventListener('click', myFunction, false);
-    } else {
-      if (element1.attachEvent) {   // IE before version 9
-        classname[i].attachEvent("click", myFunction);
-      }
+var myFunction = function(e) {
+    var attribute = e.getAttribute("data-hex");
+    if (oldElement) {
+      oldElement.textContent=oldText;
     }
+    oldElement = e;
+    oldText = e.nodeValue || e.textContent;
+    var dummy = document.createElement("input");
+      // Add it to the document
+      document.body.appendChild(dummy);
+      // Set its ID
+      dummy.setAttribute("id", "dummy_id");
+      // Output the array into it
+      document.getElementById("dummy_id").value=attribute;
+      // Select it
+      dummy.select();
+
+    try {
+        // Now that we've selected the anchor text, execute the copy command
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Copy email command was ' + msg);
+      } catch(err) {
+        console.log('Oops, unable to copy');
+      }
+
+      // Remove the selections - NOTE: Should use
+      // removeRange(range) when it is supported
+      window.getSelection().removeAllRanges();
+
+      e.textContent=newtext;
+
+      document.body.removeChild(dummy);
 };
