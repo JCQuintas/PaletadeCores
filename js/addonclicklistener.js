233ed82185a7b12955dd0c07f6247a127a7e76b2
Jose C Quintas Jr <juniorquintas@gmail.com>
2016-08-02 14:34:01 -0300

    identação

--- js/addonclicklistener.js
+++ js/addonclicklistener.js
@@ -3,36 +3,37 @@ var oldElement;
 var oldText;
 
 var myFunction = function(e) {
-    var attribute = e.getAttribute("data-hex");
-    if (oldElement) {
-      oldElement.textContent=oldText;
-    }
-    oldElement = e;
-    oldText = e.nodeValue || e.textContent;
-    var dummy = document.createElement("input");
-      // Add it to the document
-      document.body.appendChild(dummy);
-      // Set its ID
-      dummy.setAttribute("id", "dummy_id");
-      // Output the array into it
-      document.getElementById("dummy_id").value=attribute;
-      // Select it
-      dummy.select();
+  var attribute = e.getAttribute("data-hex");
+  if (oldElement) {
+    oldElement.textContent=oldText;
+  }
+  oldElement = e;
+  oldText = e.nodeValue || e.textContent;
+  var dummy = document.createElement("input");
 
-    try {
-        // Now that we've selected the anchor text, execute the copy command
-        var successful = document.execCommand('copy');
-        var msg = successful ? 'successful' : 'unsuccessful';
-        console.log('Copy email command was ' + msg);
-      } catch(err) {
-        console.log('Oops, unable to copy');
-      }
+  // Add it to the document
+  document.body.appendChild(dummy);
+  // Set its ID
+  dummy.setAttribute("id", "dummy_id");
+  // Output the array into it
+  document.getElementById("dummy_id").value=attribute;
+  // Select it
+  dummy.select();
+  
+  try {
+    // Now that we've selected the anchor text, execute the copy command
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Copy email command was ' + msg);
+  } catch(err) {
+    console.log('Oops, unable to copy');
+  }
 
-      // Remove the selections - NOTE: Should use
-      // removeRange(range) when it is supported
-      window.getSelection().removeAllRanges();
+  // Remove the selections - NOTE: Should use
+  // removeRange(range) when it is supported
+  window.getSelection().removeAllRanges();
 
-      e.textContent=newtext;
+  e.textContent=newtext;
 
-      document.body.removeChild(dummy);
+  document.body.removeChild(dummy);
 };
